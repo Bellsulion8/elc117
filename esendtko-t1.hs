990e8493ad18b058c91aa7261f4af7a81e06a7a4
Bellsulion8 <esendtko@inf.ufsm.br>
2015-03-22 18:15:29 -0300

    Update esendtko-t1.hs

--- esendtko-t1.hs
+++ esendtko-t1.hs
@@ -39,4 +39,41 @@ charFound x y
 		| x == (head y)		=	True
 		| otherwise 		= 	charFound x (tail y)
 		
+
+--Ex8 Reescreva a função anterior sem recursão, usando outras funções pré-definidas já vistas em aula:
+
+charFound2 :: Char -> String -> Bool
+charFound2 x y
+		| filter(== x) y == [x]		=	True
+		| otherwise					=	False
+
+--Ex9 Use a função de alta ordem 'zipWith' para produzir uma função que obtenha as diferenças, par a par, dos elementos de duas listas. Por exemplo: para listas de entrada [1,2,3,4] e [2,2,1,1], o resultado será [-1,0,2,3] 
+
+calc_dif :: [Int] -> [Int] -> [Int]
+calc_dif x y = zipWith (-)(x)(y)
+
+--Funções de Alta Ordem
+
+--Ex1 Dada uma lista de números, calcular 2*n+1 para cada número n contido na lista:
+
+calcula :: [Int] -> [Int]
+calcula x =  map(+1)(map(*2) x)
+
+--Ex2 Dadas duas listas X e Y de números inteiros, calcular 4*x+2*y+1 para cada par de números x e y pertencentes às listas:
+
+calcula2 :: [Int] -> [Int] -> [Int]
+calcula2 x y = map(+1)(zipWith(+)(map(*4)x)(map(*2)y))
+
+--Ex3 Dada uma lista de strings, produzir outra lista com strings de 10 caracteres, usando o seguinte esquema: strings de entrada com mais de 10 caracteres são truncadas, strings com até 10 caracteres são completadas com '.' até ficarem com 10 caracteres:
+
+
+--lista_str :: [String] -> [String]
+--lista_str x 
+
+
+--Ex4 Dada uma lista de idades, selecionar as que são maiores que 20 e, para cada uma, calcular o ano de nascimento correspondente (aproximado, considerando o ano atual):
+
+maior_20 :: [Int] -> [Int]
+maior_20 x = map(2015-)(filter(>20)x)
+		
 		
